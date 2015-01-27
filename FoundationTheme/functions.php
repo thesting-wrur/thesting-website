e5feb87839620e9a033b7da6972dbc00b00c94b4
Teddy <tkr65536@aol.com>
2015-01-27 00:29:16 -0500

    Worked on FoundationTheme - adding support for the WP Menu. Will give more detailed commit once clean up header.php more.
    Note - menu-walker.php comes directly (copy and paste) from the MIT Licensed FoundationPress theme.

--- FoundationTheme/functions.php
+++ FoundationTheme/functions.php
@@ -1,4 +1,5 @@
 <?php
+require 'foundation-press/menu-walker.php';
 //enqueue styles
 function sting_enqueue_foundation_styles() {
 	wp_register_style('sting-foundation', get_template_directory_uri().'/foundation/css/foundation.css');
@@ -6,10 +7,23 @@ function sting_enqueue_foundation_styles() {
 }
 add_action('wp_enqueue_scripts', 'sting_enqueue_foundation_styles');
 
+//enqueue scripts
+function sting_enqueue_foundation_script() {
+	wp_register_script('sting-foundation-js', get_template_directory_uri().'/foundation/js/foundation/foundation.js');
+	wp_register_script('sting-foundation-topbar-js', get_template_directory_uri().'/foundation/js/foundation/foundation.topbar.js');
+	wp_register_script('sting-fastclick', get_template_directory_uri().'/foundation/js/vendor/fastclick.js');
+	wp_enqueue_script('jquery');
+	wp_enqueue_script('sting-foundation-js');
+	wp_enqueue_script('sting-foundation-topbar-js');
+	wp_enqueue_script('sting-fastclick');
+}
+add_action('wp_enqueue_scripts', 'sting_enqueue_foundation_script');
+
 //register menu - enable a menu in appearance --> menu
 function sting_register_menu() {
 	register_nav_menu('primary', 'Primary Navigation');
 }
 add_action('init', 'sting_register_menu');
+
 
 ?>
