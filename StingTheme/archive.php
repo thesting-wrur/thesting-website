298912741924a5c8627786cd6755a8cbdcb26a0b
Teddy <tkr65536@aol.com>
2015-02-11 12:30:30 -0500

    Added previous page and next page links to the archive page (we can change their text and css classes later)

--- StingTheme/archive.php
+++ StingTheme/archive.php
@@ -21,5 +21,9 @@ if (is_category()) {
 ?>
 </div>
 <?php get_template_part('loop','');?>
+<div class="row">
+	<div class="large-4 medium-6 small-12 columns"><?php previous_posts_link('Preivous Page') ?></div>
+	<div class="large-4 medium-6 small-12 columns"><?php next_posts_link('Next Page'); ?></div>
+</div>
 <!--end archive.php stuff -->
 <?php get_footer() ?>
